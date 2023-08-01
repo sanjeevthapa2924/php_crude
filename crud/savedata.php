607f0759aea6c8ff5f0ae9df93c40cdcfe2a47dd
Sanjeev Thapa <sanjeevthapa2924@gmail.com>
2023-08-01 08:15:43 +0545

    Add files via upload

---
+++ crud/savedata.php
@@ -0,0 +1,15 @@
+<?php
+echo $stu_name = $_POST['sname'];
+echo $stu_address = $_POST['saddress'];
+echo $stu_class = $_POST['class'];
+echo $stu_phone = $_POST['sphone'];
+
+$conn = mysqli_connect("localhost", "root", "", "crud1") or die ("Connection Failed");
+$sql = "INSERT INTO student (sname,saddress,sclass,sphone) VALUES ('{$stu_name}','{$stu_address}','{$stu_class}','{$stu_phone}') ";
+$result = mysqli_query($conn, $sql) or die("query uncessful");
+
+header ("Location: http://localhost/crud/index.php");
+
+mysqli_close($conn);
+
+?>s
